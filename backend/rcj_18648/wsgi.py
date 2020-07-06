28ec5ef1114a151b94e04e0f54ec4d50f94cf84a
Crowdbotics <team@crowdbotics.com>
2020-07-06 13:45:26 +0000

    Initial commit.

---
+++ backend/rcj_18648/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rcj_18648 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rcj_18648.settings')
+
+application = get_wsgi_application()
